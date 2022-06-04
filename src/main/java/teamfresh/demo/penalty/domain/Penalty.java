c2f561d6bdf81d60559823a2fc0568ded9662af7
seongho-joo <hazzyseong@gmail.com>
2022-06-05 01:25:55 +0900

    feat: 연관관계 수정

--- src/main/java/teamfresh/demo/penalty/domain/Penalty.java
+++ src/main/java/teamfresh/demo/penalty/domain/Penalty.java
@@ -1,17 +1,24 @@
 package teamfresh.demo.penalty.domain;
 
+import static javax.persistence.FetchType.LAZY;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import teamfresh.demo.common.domain.BaseEntity;
+import teamfresh.demo.compensation.domain.Compensation;
 
 @Getter
 @Entity
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Penalty extends BaseEntity {
@@ -23,4 +30,8 @@ public class Penalty extends BaseEntity {
     private Integer cost;
 
     private String content;
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "compensation_id")
+    private Compensation compensation;
 }
