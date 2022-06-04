4c682ec420596933db7209710c9aa83a4f52b39c
seongho-joo <hazzyseong@gmail.com>
2022-06-05 01:24:44 +0900

    feat: 단방향 매핑으로 수정

--- src/main/java/teamfresh/demo/compensation/domain/Compensation.java
+++ src/main/java/teamfresh/demo/compensation/domain/Compensation.java
@@ -1,7 +1,5 @@
 package teamfresh.demo.compensation.domain;
 
-import static javax.persistence.FetchType.LAZY;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,16 +7,12 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import teamfresh.demo.common.domain.BaseEntity;
-import teamfresh.demo.penalty.domain.Penalty;
-import teamfresh.demo.voc.domain.VOC;
 
 @Getter
 @Entity
@@ -39,13 +33,6 @@ public class Compensation extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private CompensationStatus status;
-
-    @OneToOne(fetch = LAZY)
-    @JoinColumn(name = "penalty_id")
-    private Penalty penalty;
-
-    @OneToOne(mappedBy = "compensation", fetch = LAZY)
-    private VOC voc;
 
     public void updateStatus(CompensationStatus status) {
         this.status = status;
