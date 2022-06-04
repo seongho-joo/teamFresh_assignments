f99db158e2d96876df5e9cbc8cd64d1ef2075edb
seongho-joo <hazzyseong@gmail.com>
2022-06-04 23:03:53 +0900

    feat: 클레임이 존재하지 않는 예외 추가

--- src/main/java/teamfresh/demo/common/exception/ExceptionType.java
+++ src/main/java/teamfresh/demo/common/exception/ExceptionType.java
@@ -1,14 +1,17 @@
 package teamfresh.demo.common.exception;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import teamfresh.demo.compensation.exception.NotExistCompensationException;
 
 @Getter
 @RequiredArgsConstructor
 public enum ExceptionType {
-    ;
+    NOT_EXIST_COMPENSATION("클레임이 존재하지 않습니다.", BAD_REQUEST, NotExistCompensationException.class);
 
     private final String message;
     private final HttpStatus status;
---
+++ src/main/java/teamfresh/demo/compensation/exception/NotExistCompensationException.java
@@ -0,0 +1,7 @@
+package teamfresh.demo.compensation.exception;
+
+import teamfresh.demo.common.exception.BaseException;
+
+public class NotExistCompensationException extends BaseException {
+
+}
