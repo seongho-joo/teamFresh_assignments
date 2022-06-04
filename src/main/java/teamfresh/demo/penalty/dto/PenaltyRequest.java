575aa2c99dbc271d32c188cf692a1876e2bb8f4d
seongho-joo <hazzyseong@gmail.com>
2022-06-05 01:27:29 +0900

    feat: 패널티 등록 request Dto 생성

---
+++ src/main/java/teamfresh/demo/penalty/dto/PenaltyRequest.java
@@ -0,0 +1,30 @@
+package teamfresh.demo.penalty.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import teamfresh.demo.compensation.domain.Compensation;
+import teamfresh.demo.penalty.domain.Penalty;
+
+@Getter
+@AllArgsConstructor
+public class PenaltyRequest {
+
+    @NotBlank(message = "패널티 내용을 입력해주세요.")
+    private String content;
+
+    @NotNull(message = "배상 금액을 입력해주세요.")
+    private Integer cost;
+
+    @NotNull(message = "배상 청구를 선택해주세요.")
+    private Long compensationId;
+
+    public Penalty toPenalty(Compensation compensation) {
+        return Penalty.builder()
+            .compensation(compensation)
+            .content(content)
+            .cost(cost)
+            .build();
+    }
+}
