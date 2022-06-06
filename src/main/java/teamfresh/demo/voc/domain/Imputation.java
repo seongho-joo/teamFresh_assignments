f29a540ff9b32fa4d0b11188fcd0a726a710a0da
seongho-joo <hazzyseong@gmail.com>
2022-06-06 15:42:47 +0900

    feat: 귀책사 코드 및 JsonCreator 구현

--- src/main/java/teamfresh/demo/voc/domain/Imputation.java
+++ src/main/java/teamfresh/demo/voc/domain/Imputation.java
@@ -1,13 +1,26 @@
 package teamfresh.demo.voc.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum Imputation {
-    DELIVERY("배송사"),
-    CLIENT("고객사");
+    DELIVERY(1, "배송사"),
+    CLIENT(2, "고객사");
 
-    private final String description;
+    private final Integer code;
+    private final String name;
+
+    @JsonCreator
+    public static Imputation from(Integer code) {
+        for (Imputation value : Imputation.values()) {
+            if (Objects.equals(value.getCode(), code)) {
+                return value;
+            }
+        }
+        return null;
+    }
 }
