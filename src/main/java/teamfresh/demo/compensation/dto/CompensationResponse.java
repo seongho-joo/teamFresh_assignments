b0bd7ff72024673646b913422b3262c61896afeb
seongho-joo <hazzyseong@gmail.com>
2022-06-06 17:54:08 +0900

    feat: 배상 청구 상세 조회 응답 생성

--- src/main/java/teamfresh/demo/compensation/dto/CompensationResponse.java
+++ src/main/java/teamfresh/demo/compensation/dto/CompensationResponse.java
@@ -1,25 +1,50 @@
 package teamfresh.demo.compensation.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import teamfresh.demo.compensation.domain.Compensation;
+import teamfresh.demo.compensation.domain.CompensationStatus;
+import teamfresh.demo.penalty.domain.Penalty;
+import teamfresh.demo.voc.domain.VOC;
+import teamfresh.demo.voc.dto.VocDto;
+
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CompensationResponse {
 
     private Long id;
     private String title;
     private String content;
-    private String status;
+    private CompensationStatus status;
+    private VocDto voc;
+    private Integer penaltyCost;
+    private LocalDateTime createdAt;
 
-    public static CompensationResponse from(Compensation compensation) {
+    @QueryProjection
+    public CompensationResponse(Long id, String title, String content,
+        CompensationStatus status, VocDto voc,Integer penaltyCost, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.status = status;
+        this.voc = voc;
+        this.penaltyCost = penaltyCost;
+        this.createdAt = createdAt;
+    }
+
+    public static CompensationResponse of(Compensation compensation, VOC voc, Penalty penalty) {
         return new CompensationResponse(
             compensation.getId(),
             compensation.getTitle(),
             compensation.getContent(),
-            compensation.getStatus().getDescription()
+            compensation.getStatus(),
+            VocDto.from(voc),
+            penalty.getCost(),
+            compensation.getCreatedAt()
         );
     }
 }
