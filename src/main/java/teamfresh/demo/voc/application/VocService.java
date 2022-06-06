9b800e5f0bed11070f669857bba2b35df1b04824
seongho-joo <hazzyseong@gmail.com>
2022-06-06 18:33:59 +0900

    feat: 배송기사 패널티 확인 서비스 구현

--- src/main/java/teamfresh/demo/voc/application/VocService.java
+++ src/main/java/teamfresh/demo/voc/application/VocService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import teamfresh.demo.compensation.domain.Compensation;
 import teamfresh.demo.compensation.domain.repository.CompensationRepository;
 import teamfresh.demo.compensation.exception.NotExistCompensationException;
+import teamfresh.demo.voc.domain.VOC;
 import teamfresh.demo.voc.domain.repository.VocRepository;
 import teamfresh.demo.voc.dto.VocRequest;
 import teamfresh.demo.voc.dto.VocResponse;
@@ -30,11 +31,18 @@ public class VocService {
         vocRepository.save(request.toVoc(compensation));
     }
 
+    @Transactional
     public VocResponse getVoc(Long id) {
         return vocRepository.getVoc(id).orElseThrow(NotExistVocException::new);
     }
 
     public List<VocsResponse> getVocLists() {
         return VocsResponse.from(vocRepository.findAll());
+    }
+
+    @Transactional
+    public void driverConfirmed(Long id) {
+        VOC voc = vocRepository.findById(id).orElseThrow(NotExistVocException::new);
+        voc.updateDriverConfirmationStatus();
     }
 }
