3cc2c07a02de39ebc1823d587ccdd5f112db102c
seongho-joo <hazzyseong@gmail.com>
2022-06-06 15:37:07 +0900

    test: querydsl 테스트 환경 구성

--- src/test/java/teamfresh/demo/BaseRepositoryTest.java
+++ src/test/java/teamfresh/demo/BaseRepositoryTest.java
@@ -7,12 +7,15 @@ import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import teamfresh.demo.config.TestQuerydslConfig;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ActiveProfiles("local")
 @DataJpaTest
+@Import(TestQuerydslConfig.class)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public @interface BaseRepositoryTest {
 
---
+++ src/test/java/teamfresh/demo/config/TestQuerydslConfig.java
@@ -0,0 +1,19 @@
+package teamfresh.demo.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class TestQuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
