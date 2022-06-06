43109da54f89896f35819eaba36e93fb4330b923
seongho-joo <hazzyseong@gmail.com>
2022-06-06 18:33:47 +0900

    feat: 배송기사 패널티 확인 응답 메시지 추가

--- src/main/java/teamfresh/demo/voc/ui/VocResponseMessage.java
+++ src/main/java/teamfresh/demo/voc/ui/VocResponseMessage.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum VocResponseMessage {
     CREATE_VOC("voc 등록 성공"),
     GET_VOC("voc 상세 조회 성공"),
-    GET_VOC_LIST("voc 목록 조회 성공");
+    GET_VOC_LIST("voc 목록 조회 성공"),
+    UPDATE_DRIVER_CONFIRMATION("배송기사 내용 확인");
 
     private final String message;
 }
